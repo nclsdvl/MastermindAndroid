852a9a548f4f480c0cac8c09197242639882179a
nicolas duval <nicolas.duval@maformationenligne.bzh>
2019-01-22 17:23:25 +0100

    TODO controller

---
+++ app/src/main/java/com/nicolas/mastermindandroid/ControllerMastermind.java
@@ -0,0 +1,5 @@
+package com.nicolas.mastermindandroid;
+
+public class ControllerMastermind {
+
+}
--- app/src/main/java/com/nicolas/mastermindandroid/mastermind.java
+++ app/src/main/java/com/nicolas/mastermindandroid/mastermind.java
@@ -1,34 +1,142 @@
 package com.nicolas.mastermindandroid;
 
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 
 public class mastermind extends AppCompatActivity {
+
+
+
+    int countX = 0;
+    int countY = 9;
+    private TableLayout tableLayout;
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mastermind);
 
         View myRectangleViewVert = findViewById(R.id.myRectangleViewVert);
-        myRectangleViewVert.setBackgroundColor(Color.argb(255,50,220,50));
+        myRectangleViewVert.setBackgroundResource(R.drawable.cerclepropositionvert);
 
         View myRectangleViewRouge = findViewById(R.id.myRectangleViewRouge);
-        myRectangleViewRouge.setBackgroundColor(Color.argb(255,200,0,0));
+        myRectangleViewRouge.setBackgroundResource(R.drawable.cerclepropositionrouge);
 
         View myRectangleViewBleu = findViewById(R.id.myRectangleViewBleu);
-        myRectangleViewBleu.setBackgroundColor(Color.argb(255,0,0,200));
+        myRectangleViewBleu.setBackgroundResource(R.drawable.cerclepropositionbleu);
 
         View myRectangleViewJaune = findViewById(R.id.myRectangleViewJaune);
-        myRectangleViewJaune.setBackgroundColor(Color.argb(255,255,230,0));
+        myRectangleViewJaune.setBackgroundResource(R.drawable.cerclepropositionjaune);
 
         View myRectangleViewViolet = findViewById(R.id.myRectangleViewViolet);
-        myRectangleViewViolet.setBackgroundColor(Color.argb(255,138,43,226));
+        myRectangleViewViolet.setBackgroundResource(R.drawable.cerclepropositionviolet);
+
+//
+//        //monTab[0] = R.id.cercleVideProposition_10_4;
+//
+        tableLayout = (TableLayout) findViewById(R.id.tableLayout);
+//
+
+        /*
+        for (int i=tableLayout.getChildCount() - 1 ; i > -1; i--){
+            for(int y=0; y<4; y++){
+                View monCercle = ((TableRow)tableLayout.getChildAt(i)).getChildAt(y);
+                Log.d("AJOUT", monCercle.toString());
+                monTab[i*4+y] = monCercle;
+            }
+
+        }
+*/
+
+
+       myRectangleViewViolet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((TableRow)tableLayout.getChildAt(countY)).getChildAt(countX).setBackgroundResource(R.drawable.cerclepropositionviolet);
+
+                countX ++;
+                if (countX == 4){
+                    countX =0;
+                    countY --;
+
+                    //Correnspond à la fin de la partie
+                    if (countY < 0) countY = 0;
+                }
+            }
+
+        });
+        myRectangleViewJaune.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((TableRow)tableLayout.getChildAt(countY)).getChildAt(countX).setBackgroundResource(R.drawable.cerclepropositionjaune);
+
+                countX ++;
+                if (countX == 4){
+                    countX =0;
+                    countY --;
+
+                    //Correnspond à la fin de la partie
+                    if (countY < 0) countY = 0;
+                }
+            }
+
+        });
+        myRectangleViewBleu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((TableRow)tableLayout.getChildAt(countY)).getChildAt(countX).setBackgroundResource(R.drawable.cerclepropositionbleu);
+
+                countX ++;
+                if (countX == 4){
+                    countX =0;
+                    countY --;
+
+                    //Correnspond à la fin de la partie
+                    if (countY < 0) countY = 0;
+                }
+            }
+
+        });
+        myRectangleViewRouge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((TableRow)tableLayout.getChildAt(countY)).getChildAt(countX).setBackgroundResource(R.drawable.cerclepropositionrouge);
+
+                countX ++;
+                if (countX == 4){
+                    countX =0;
+                    countY --;
+
+                    //Correnspond à la fin de la partie
+                    if (countY < 0) countY = 0;
+                }
+            }
+
+        });
+        myRectangleViewVert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((TableRow)tableLayout.getChildAt(countY)).getChildAt(countX).setBackgroundResource(R.drawable.cerclepropositionvert);
+
+                countX ++;
+                if (countX == 4){
+                    countX =0;
+                    countY --;
+
+                    //Correnspond à la fin de la partie
+                    if (countY < 0) countY = 0;
+                }
+            }
+
+        });
+
     }
-
-    // YourView.setBackgroundColor(Color.argb(255, 255, 255, 255));
-
-
 }
